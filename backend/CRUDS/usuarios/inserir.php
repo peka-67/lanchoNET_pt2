0fc0a7c42fc6598237432590004d7c8448035a1d
peka-67 <peka.alves22@gmail.com>
2026-08-28 21:34:23 -0300

    commit

--- backend/CRUDS/usuarios/inserir.php
+++ backend/CRUDS/usuarios/inserir.php
@@ -3,14 +3,12 @@ include '../conexao.php';
 //receber os dados dos names do frontend
     $nome = mysqli_real_escape_string($conexao, $_POST['nome']);
     $email = mysqli_real_escape_string($conexao, $_POST['email']);
-    $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
     $cargo = mysqli_real_escape_string($conexao, $_POST('cargo'));
     $ativo = mysqli_real_escape_string($conexao, $_POST['ativo']);
     $efetivado_em = mysqli_real_escape_string($conexao, $_POST['efetivado_em']);
     $id_restaurante = mysqli_real_escape_string($conexao, $_POST['id_restaurante']);
     $origem = $_POST['origem'] ?? '';
-
-    $hash = password_hash($senha, PASSWORD_DEFAULT);
 
 //inserção em SQL - linguagem do banco
 $sql = "INSERT INTO usuarios (nome, email, senha, cargo, ativo, efetivado_em, id_restaurante) VALUES ('$nome','$email','$hash','$cargo', '$ativo', '$efetivado_em', '$id_restaurante')";
--- backend/meuLogin.php
+++ backend/meuLogin.php
@@ -1,29 +1,39 @@
 <?php
+
+session_start();
 //conectar com o banco 
 include './conexao.php';
 
 //receber o email e senha do front end por requisição
-$email = $_REQUEST['email'];
-$senha = $_REQUEST['senha'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
-//SQL que busca no banco um usuário com o email e senha recebidos 
-$sql = "SELECT * FROM restaurantes WHERE email = '$email' AND senha = '$senha'";
-//executar a SQL
-$resultado = mysqli_query($conexao, $sql);
-//pegar valores das colunas do banco
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: ../index.php');
+    exit;
+}
+
+$sql = "SELECT * FROM restaurantes WHERE email = 'maceciyema@gmail.com' LIMIT 1";
+
+// prepara o php para uma execução de instrução do codigo sql
+$stmt = mysqli_prepare($conexao, $sql);
+
+// vincula variaveis aos marcadores de parametros (?)
+mysqli_stmt_bind_param($stmt, "s", $email);
+
+mysqli_stmt_execute($stmt);
+
+$resultado = mysqli_stmt_get_result($stmt);
+
 $colunas = mysqli_fetch_assoc($resultado);
 
-echo $colunas['nome'];
-
-
-if (mysqli_num_rows($resultado) > 0) {
-    session_start();
+if ($colunas && password_verify($senha, $colunas['senha'])) {
+    session_regenerate_id(true);
 
     $_SESSION['usuario'] = $colunas['nome'];
     $_SESSION['email'] = $colunas['email'];
-    $_SESSION['senha'] = $colunas['senha'];
-    $_SESSION['usuario_id'] = $colunas['id'];
-
+    $_SESSION['id'] = $colunas['id'];
+    
     header('location:../adm/criarRestaurante.php');
 } 
 
