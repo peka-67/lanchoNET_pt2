756b4ab79ae1a40cf6ecdaa79a1397bd02e54ad7
peka-67 <peka.alves22@gmail.com>
2026-09-02 22:06:30 -0300

    arrumei o login

--- backend/logar.php
+++ backend/logar.php
@@ -1,52 +1,77 @@
 <?php
 
 session_start();
-//conectar com o banco 
 include './conexao.php';
-
-//receber o email e senha do front end por requisição
-$email = $_POST['email'];
-$senha = $_POST['senha'];
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('Location: ../index.php');
     exit;
 }
 
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
 $sql = "SELECT * FROM usuarios WHERE email = ? LIMIT 1";
 
-// prepara o php para uma execução de instrução do codigo sql
 $stmt = mysqli_prepare($conexao, $sql);
 
-// vincula variaveis aos marcadores de parametros (?)
 mysqli_stmt_bind_param($stmt, "s", $email);
 
 mysqli_stmt_execute($stmt);
 
 $resultado = mysqli_stmt_get_result($stmt);
 
-$colunas = mysqli_fetch_assoc($resultado);
+$usuario = mysqli_fetch_assoc($resultado);
 
-if ($colunas && password_verify($senha, $colunas['senha'])) {
+if ($usuario) {
+
+    if (!password_verify($senha, $usuario['senha'])) {
+        header('Location: ../loginErrado.php');
+        exit;
+    }
+
     session_regenerate_id(true);
 
-    $_SESSION['usuario'] = $colunas['nome'];
-    $_SESSION['email'] = $colunas['email'];
-    $_SESSION['usuario_id'] = $colunas['id'];
-    $_SESSION['cargo'] = $colunas['cargo'];
-    $_SESSION['idRestaurante'] = $colunas['id_restaurante']; 
+    $_SESSION['tipo'] = 'usuario';
+    $_SESSION['usuario'] = $usuario['nome'];
+    $_SESSION['email'] = $usuario['email'];
+    $_SESSION['usuario_id'] = $usuario['id'];
+    $_SESSION['cargo'] = $usuario['cargo'];
+    $_SESSION['idRestaurante'] = $usuario['id_restaurante'];
 
-    if ($colunas['cargo'] == 'admin' ) {
-        header('location:../dashboard.php');
-    }
-    elseif ($colunas['cargo'] == 'cozinha') {
-        header('location:../cozinha.php');
-    }
-    else {
-        header('location:../caixa.php');
+    if ($usuario['cargo'] === 'admin') {
+        header('Location: ../dashboard.php');
+        exit;
     }
 
-} 
-else {
-    header('location:../loginErrado.php');
+    if ($usuario['cargo'] === 'cozinha') {
+        header('Location: ../cozinha.php');
+        exit;
+    }
+
+    header('Location: ../caixa.php');
+    exit;
 }
+
+
+$sql2 = "SELECT * FROM administrador WHERE email = ? LIMIT 1";
+
+$stmt2 = mysqli_prepare($conexao, $sql2);
+mysqli_stmt_bind_param($stmt2, "s", $email);
+mysqli_stmt_execute($stmt2);
+$resultado2 = mysqli_stmt_get_result($stmt2);
+$admin = mysqli_fetch_assoc($resultado2);
+if ($admin) {
+    if (!password_verify($senha, $admin['senha'])) {
+        header('location: ../loginErrado.php');
+        exit;
+    }
+    session_regenerate_id(true);
+    $_SESSION['admin_email'] = $admin['email'];
+    $_SESSION['admin_id'] = $admin['id'];
+    header('location: ../adm/criarRestaurante');
+    exit;
+}
+
+header('location:../loginErrado.php');
+exit;
